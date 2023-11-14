d0d6813feb8679e8ebb549cefcdbf398c42ad564
OmegaaDeveloperr <100985197+OmegasGithub@users.noreply.github.com>
2023-11-13 22:54:21 -0500

    Rebuild

--- MegaModLoaderUMLcustomBuild/UnrealModLoader-main/UnrealEngineModLoader/UnrealEngineModLoader/GameInfo/GameInfo.cpp
+++ MegaModLoaderUMLcustomBuild/UnrealModLoader-main/UnrealEngineModLoader/UnrealEngineModLoader/GameInfo/GameInfo.cpp
@@ -58,7 +58,7 @@ void SetupProfile(std::string Path)
 		freopen("CON", "w", LOG_STREAM);
 		Log::Info("Created by ~Russell.J Release V %sa", MODLOADER_VERSION);
 		Log::Dmg("Modified by DmgVol");
-		Log::Omg("Modified even further by OmegaDev");
+		Log::Omg("Modified even further by OmegaDev & MasterAirscratch");
 	}
 
 	if (LoaderInfo.getAs<int>("DEBUG", "DX12", 0) == 1) {
